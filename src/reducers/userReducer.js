d0ad0ed5a243003238afa13ca0d013493c15406e
parvizwpf <30858096+parvizwpf@users.noreply.github.com>
2022-09-07 10:50:27 +0430

    init

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_VALUE = {
+  user: null,
+};
+
+const userReducer = (state = INITIAL_VALUE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
